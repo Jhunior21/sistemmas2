206984f1db4e21e5c7fe7d69bab89ddc909b3701
junior <jhuniorjlking@gmail.com>
2019-08-09 20:28:11 -0500

    hola

---
+++ Suma/CapaNegocio/Negocio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class Negocio
+    {
+        Datos dts = new Datos();
+        public double N_Suma(Entidad obj)
+        {
+            return dts.D_Suma(obj);
+        }
+       
+    }
+}
